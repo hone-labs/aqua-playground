4ce0ad368cc973c986d4f9f2ee7b5477b5ae9ff0
Ashley Davis <ashley@codecapers.com.au>
2022-02-15 08:09:09 +1000

    Removed the router.

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
-import { Layout } from "./pages/layout";
-import { Page1 } from "./pages/page-1";
-import { Page2 } from "./pages/page-2";
 
 class App extends React.Component {
     render() {
         return (
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route path="page-1" element={<Page1 />} />
-                        <Route path="page-2" element={<Page2 />} />
-                        <Route path="/" element={<Navigate replace to="/page-1" />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
+            <div>
+                Hello world!
+            </div>
         );                
     }
 }
